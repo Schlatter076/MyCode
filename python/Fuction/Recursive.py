c73c91fdfbd3992df3e126664ceb14846cd9d442
Schlatter076 <schlatter076@163.com>
2018-06-30 14:46:15 +0800

    求解汉诺塔

--- python/Fuction/Recursive.py
+++ python/Fuction/Recursive.py
@@ -11,7 +11,7 @@ print('8! = ', fact(8))
 使用递归要小心栈(stack)溢出
 可通过尾递归优化--类似循环语句
 尾递归是指，在函数返回的时候，调用自身本身，并且，return语句不能包含表达式
-可惜解释器没有做尾递归优化，仍然回溢出
+可惜解释器没有做尾递归优化，仍然会溢出
 '''
 print('优化阶乘函数：')
 def fact(n):
@@ -21,3 +21,17 @@ def fact_1(num, product):
         return product
     return fact_1(num - 1, num * product)
 print('10! = ', fact_1(10, 1))
+print('求解汉诺塔')
+'''
+从a将n个盘子借助b移动到c，小盘子上不能放大盘子
+先把n-1个盘子移动到b，再把第n个盘子从a移到c
+最后从b把n-1个盘子移动到c
+'''
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '-->', c)
+    else:
+        move(n - 1, a, c, b)
+        move(1, a, b, c)
+        move(n - 1, b, a, c)
+move(6, 'A', 'B', 'C')
