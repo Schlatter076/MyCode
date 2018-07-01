03a4430985648709e5dc88509e23a0335ea4e22a
Schlatter076 <schlatter076@163.com>
2018-07-01 12:05:03 +0800

    用查表法优化16进制转换

--- java/Array/Conversion.java
+++ java/Array/Conversion.java
@@ -7,6 +7,7 @@
 public class Conversion {
   public static void main(String[] args) {
     toHex(60);
+    toHex_1(60);
   }
   public static void toHex(int num) {
     System.out.printf("求%d的16进制形式：\n", num);
@@ -21,5 +22,29 @@ public class Conversion {
         System.out.print(x);
       num = num >>> 4;
     }
+    System.out.println();
+  }
+  //查表法
+  public static void toHex_1(int num) {
+    System.out.printf("求%d的16进制形式(查表法)：\n", num);
+    if(num == 0) {
+      System.out.println(num);
+      return;
+    }
+    char[] hex = {'0', '1', '2', '3',
+                  '4', '5', '6', '7',
+                  '8', '9', 'A', 'B',
+                  'C', 'D', 'E', 'F'};
+    char[] arr = new char[8];//最多有8个数
+    int point = arr.length;
+    while(num != 0) {
+      int temp = num & 15;
+      //建一个表arr，从尾部存运算后的数值
+      arr[--point] = hex[temp];
+      num = num >>> 4;
+    }
+    for(int i = point; i < arr.length; i++)
+      System.out.print((char)(arr[i]));
+    System.out.println();
   }
 }
